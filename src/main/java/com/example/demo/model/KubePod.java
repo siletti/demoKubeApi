34f0dd03ddd9cbdc18b925dc76e2b037f5611c12
Alberto Siletti <alberto.siletti@consultant.vodafoneomnitel.it>
2022-04-08 17:31:45 +0200

    add model

---
+++ src/main/java/com/example/demo/model/KubePod.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class KubePod implements Serializable {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("applicationGroup")
+    private String applicationGroup;
+
+    @JsonProperty("runningPodsCount")
+    private int runningPodsCount;
+
+}
+
